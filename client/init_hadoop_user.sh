a4daa5b71643de670e3432c27023d07f05fb675f
yongxing <yongxing@e1a62a3c-03e2-43f2-b1ff-b74bda9a2190>
2013-07-30 01:46:03 +0000

    backport D1154
    Summary: Ref T524
    
    Test Plan: N/A
    
    Reviewers: wuzesheng
    
    Reviewed By: wuzesheng
    
    CC: infra-dev
    
    Maniphest Tasks: T524
    
    Differential Revision: http://phabricator.n.miliao.com/D1188
    
    git-svn-id: https://code.miliao.com/repos/xiaomi-miliao/trunk/src/cloud/infra/minos@103724 e1a62a3c-03e2-43f2-b1ff-b74bda9a2190

--- client/init_hadoop_user.sh
+++ client/init_hadoop_user.sh
@@ -18,11 +18,12 @@ if [[ "$user" =~ hbase.* ]]; then
   $HDFS -chown $user /hbase
 elif [[ "$user" =~ yarn.* ]]; then
   $HDFS -mkdir -p /tmp/hadoop-yarn/staging/history
-  $HDFS -chown $user /tmp/hadoop-yarn
+  $HDFS -chown $user:hadoop /tmp/hadoop-yarn
   $HDFS -chmod -R 777 /tmp/hadoop-yarn/staging
+  $HDFS -chown $user:hadoop /tmp/hadoop-yarn/staging/history
 
   $HDFS -mkdir -p /var/log/hadoop-yarn
-  $HDFS -chown $user /var/log/hadoop-yarn
+  $HDFS -chown $user:hadoop /var/log/hadoop-yarn
 else
   $HDFS -mkdir -p /user/$user
   $HDFS -chown $user /user/$user
