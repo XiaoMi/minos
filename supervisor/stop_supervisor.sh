473209f661c1f8ce54a4a249c531319d3fa0d264
yongxing <yongxing@xiaomi.com>
2013-11-07 19:58:22 +0800

    fix bug in stop_supervisor.sh
    Summary: Ref T671
    
    Test Plan: N/A
    
    Reviewers: wuzesheng
    
    Reviewed By: wuzesheng
    
    Maniphest Tasks: T671
    
    Differential Revision: http://phabricator.n.miliao.com/D1797

--- supervisor/stop_supervisor.sh
+++ supervisor/stop_supervisor.sh
@@ -4,7 +4,7 @@ pid=`ps aux|grep supervisord.py | grep -v grep | awk '{print $2}'`
 
 kill $pid && \
 while [ 1 ]; do
-  if netstat -nlap 2>/dev/null |grep 9001 |grep LISTEN; then
+  if netstat -nlap 2>/dev/null |grep ":9001" |grep LISTEN; then
     sleep 1
     echo "Wait for supervisor exiting..."
   else
