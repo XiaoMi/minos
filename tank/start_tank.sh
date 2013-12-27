10a6e46356692ccdab883f4324258597059c1ee8
yongxing <yongxing@xiaomi.com>
2013-12-27 16:44:27 +0800

    change tank start sh
    Summary: T671
    
    Test Plan: N/A
    
    Reviewers: wuzesheng
    
    Reviewed By: wuzesheng
    
    Differential Revision: http://phabricator.n.miliao.com/D2587

--- tank/start_tank.sh
+++ tank/start_tank.sh
@@ -6,6 +6,5 @@ if ! [ -e $db_file ] || [ -z "`cat $db_file`" ]; then
   ./manage.py syncdb
 fi
 
-hostname=`hostname --fqdn`
-local_ip=`host $hostname | awk '{print $NF}'`
+local_ip=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 nohup ./manage.py runserver $local_ip:8000 1>tank.log 2>&1 &
