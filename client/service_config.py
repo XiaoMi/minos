1d82319fc72ebae944b159e14bfe2a4c95e50362
yongxing <xyong.ac@gmail.com>
2014-02-19 10:04:32 +0800

    fix configobj.Section type
    Summary: Ref T671
    
    Test Plan: N/A
    
    Reviewers: wuzesheng
    
    Reviewed By: wuzesheng
    
    Maniphest Tasks: T671
    
    Differential Revision: http://phabricator.n.miliao.com/D3027

--- client/service_config.py
+++ client/service_config.py
@@ -573,7 +573,7 @@ class ServiceConfig:
     '''
     generated_files = {}
     for file_name, file_dict in config_section_dict.iteritems():
-      if type(file_dict) == dict:
+      if isinstance(file_dict, dict):
         for key, value in file_dict.iteritems():
           if key.find('%') != -1:
             file_dict.pop(key)
