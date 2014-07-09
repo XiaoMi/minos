53b143f60001b2d08f031148c52462c85ff83b8f
Hangjun Ye <yehangjun@xiaomi.com>
2014-07-09 17:05:31 +0800

    Added machine models and admin interface.
    Summary: Ref T1534
    
    Test Plan: http://10.237.113.12:8000/admin/machine/machine/
    
    Reviewers: yongxing
    
    Reviewed By: yongxing
    
    Subscribers: infra-dev
    
    Maniphest Tasks: T1534
    
    Differential Revision: https://phabricator.d.xiaomi.net/D4984

--- owl/machine/admin.py
+++ owl/machine/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Machine
+
+
+class MachineAdmin(admin.ModelAdmin):
+  list_display = ('hostname', 'ip', 'idc', 'rack', 'cores', 'ram',
+      'disks', 'disk_capacity', 'ssds', 'ssd_capacity', )
+  list_filter = ('idc', 'rack', )
+  ordering = ('hostname', )
+
+
+admin.site.register(Machine, MachineAdmin)
--- owl/machine/models.py
+++ owl/machine/models.py
@@ -4,6 +4,33 @@ from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
 
+class Machine(models.Model):
+  # Identifier
+  hostname = models.CharField(max_length=64, unique=True)
+  ip = models.IPAddressField(unique=True)
+
+  # Location
+  idc = models.CharField(max_length=8)
+  rack = models.CharField(max_length=8)
+
+  # Capacity
+  cores = models.IntegerField()
+  ram = models.IntegerField(
+      help_text='RAM in G bytes')
+  disks = models.IntegerField(
+      help_text='Number of disks')
+  disk_capacity = models.IntegerField(
+      help_text='Capaciy of each disk in G bytes')
+  ssds = models.IntegerField(default=0,
+      help_text='Number of SSDs')
+  ssd_capacity = models.IntegerField(default=0,
+      help_text='Capaciy of each SSD in G bytes')
+
+  # Trace record change
+  create_time = models.DateTimeField(auto_now_add=True)
+  update_time = models.DateTimeField(auto_now=True)
+
+
 @receiver(pre_save, sender=auth_models.User)
 def auth_user_pre_save(sender, instance=None, **kwargs):
   instance.is_staff = True
